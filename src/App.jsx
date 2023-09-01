f5213de76ab539d56fd1ca9dfae935134cbf3178
fatimahaj0 <fatima_haj00@outlook.com>
2023-09-01 15:33:27 +0200

    message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
+
+// import ProductList from "./pages/ProductList";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
